cfca10e7c91bf5a8e9b9edb8fa40850fe71551ed
Mongezi Mncube <mocussmncube@gmail.com>
2023-10-23 18:41:07 +0200

    Created an empty string to enable the input to be cleared after thet submit btn is clicked

--- script.js
+++ script.js
@@ -1,12 +1,30 @@
-const  myLeads = []
-let inputEl = document.getElementById("input-el");
+const  myLeads = [ ]
+const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn")
+const ulEl = document.getElementById("ulEl");
 
-inputEl = "Hello";
 
 inputBtn.addEventListener("click",() => {
-     myLeads.push("www.awesomelead.com")
+     myLeads.push(inputEl.value)
      console.log(myLeads);
+
+     inputEl.value = " ";
+
+     renderLeads();     
 });
+
+inputBtn.addEventListener()
+// Wrapped the code under the function below.
+function renderLeads(){
+     let listItems = " "; 
+     for (let i = 0; i < myLeads.length; i++) {
+
+     listItems += "<li>" + myLeads[i] + "</li>"
+     console.log(listItems);
+ }
+
+ulEl.innerHTML = listItems
+     
+}
 
 
