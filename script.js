317b50b2927721570dcd58bf65c3636b307f072f
Mongezi Mncube <mocussmncube@gmail.com>
2023-10-23 17:09:48 +0200

    Creating an input button

---
+++ script.js
@@ -0,0 +1,12 @@
+const  myLeads = []
+let inputEl = document.getElementById("input-el");
+const inputBtn = document.getElementById("input-btn")
+
+inputEl = "Hello";
+
+inputBtn.addEventListener("click",() => {
+     myLeads.push("www.awesomelead.com")
+     console.log(myLeads);
+});
+
+
